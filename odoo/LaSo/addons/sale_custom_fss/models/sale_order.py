455053c6c3e2120eabb79052104ae2594a878438
Antonio Silva <gsisa.asilva@gmail.com>
2021-04-11 16:50:57 -0500

    [IMP] sale_custom_fss: Improvement client_order_ref automatic fill

--- odoo/LaSo/addons/sale_custom_fss/models/sale_order.py
+++ odoo/LaSo/addons/sale_custom_fss/models/sale_order.py
@@ -7,9 +7,9 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    margin_percent = fields.Float(string="Margin(%)", compute='_product_margin')
+    margin_percent = fields.Float(string="Margin(%)", compute='_product_margin_percent')
 
     @api.depends('margin','amount_total')
-    def _product_margin(self):
+    def _product_margin_percent(self):
         for order in self:
             order.margin_percent = order.margin / order.amount_total if order.amount_total else 0.0
